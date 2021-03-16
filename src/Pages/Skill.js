1a85c773493a89ede1c8523849d0f159504f388e
chhun93 <chhun3830@gmail.com>
2021-03-16 23:18:07 +0900

    일단 이미지 올리기

--- src/Pages/Skill.js
+++ src/Pages/Skill.js
@@ -4,11 +4,27 @@ import "../Styles/Skill.css";
 
 export default class Skill extends Component {
   render() {
+    const skillItems = [
+      "/Images/frame_Express.png",
+      "/Images/frame_Net.png",
+      "/Images/frame_NodeJs.png",
+      "/Images/frame_React.png",
+      "/Images/frame_Vb.png",
+      "/Images/language_C.png",
+      "/Images/language_Cs.png",
+      "/Images/language_Cpp.png",
+      "/Images/language_Cpp.png",
+      "/Images/language_CssHtml.png",
+      "/Images/language_Js.png",
+    ];
+    const skillItem = skillItems.map((skill) => (
+      <img src={process.env.PUBLIC_URL + skill} className="img" alt="??"/>
+    ));
     return (
       <div className="skill">
         skill
+        {skillItem}
       </div>
     );
   }
 }
-
