ecb98f0b97cd09674710d8db32d3527c594c5ddf
chhun93 <chhun3830@gmail.com>
2021-03-25 23:22:48 +0900

    description 빨리구현하자..

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -6,17 +6,24 @@ import "../Styles/Description.css";
 export default class Description extends Component {
   render() {
     const { skillSet, path } = this.props;
-    const arrData = descr.map((item, index) => {
+    const arrDataContent = descr.map((item) => {
       if (item.name === path) {
         return item.content;
       }
     });
+    const arrDataAddress = descr.map((item) => {
+      if (item.name === path) {
+        return item.address;
+      }
+    });
+    const itemContent = arrDataContent;
+    const itemAddress = arrDataAddress;
     return (
       <div className="description">
         {skillSet}
-        <div>
-          {arrData}
-        </div>
+        <p>
+          {`${itemContent}\n  \n  \n${itemAddress}`}
+        </p>
       </div>
     );
   }
